0fd64d0c10574048d74eaed7181f0d3167b069a4
Oscar Curto <oscarcj98@gmail.com>
2022-09-24 20:24:25 +0200

    Corregido código Attack

--- src/interfaces/Attack.java
+++ src/interfaces/Attack.java
@@ -2,10 +2,5 @@ package interfaces;
 
 public interface Attack {
 
-    //int id, string name, int hp, boolean isAlive
-
-    int getId (int id);
-    String getName(String name);
-    int getHp(int hp);
-    boolean isAlive(boolean isAlive);
+    void attack();
 }
