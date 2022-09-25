371b7d88f4d483533eea96801e87ffb6e9290240
Oscar Curto <oscarcj98@gmail.com>
2022-09-25 11:21:23 +0200

    Editado código Character

--- src/classes/Character.java
+++ src/classes/Character.java
@@ -2,31 +2,22 @@ package classes;
 
 import interfaces.Attack;
 
-import java.util.ArrayList;
-
 public abstract class Character implements Attack {
 
-    private int id;
+    private final int id = 0;
     private String name;
     private int hp;
     private boolean isAlive;
 
-    ArrayList<Character> character = new ArrayList<>();
-
-    public Character(int id, String name, int hp, boolean isAlive, ArrayList<Character> character) {
-        this.id = id;
-        this.name = name;
-        this.hp = hp;
-        this.isAlive = isAlive;
-        this.character = character;
+    public Character(int id, String name, int hp, boolean isAlive) {
+        this.id = id++;
+        setName(name);
+        setHp(hp);
+        setAlive(true);
     }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -42,7 +33,7 @@ public abstract class Character implements Attack {
     }
 
     public void setHp(int hp) {
-        this.hp = hp;
+
     }
 
     public boolean isAlive() {
@@ -53,15 +44,6 @@ public abstract class Character implements Attack {
         isAlive = alive;
     }
 
-    public ArrayList<Character> getCharacter() {
-        return character;
-    }
-
-    public void setCharacter(ArrayList<Character> character) {
-        this.character = character;
-    }
-
-    public abstract void Attack();
-
+    public abstract void attack();
 
 }
