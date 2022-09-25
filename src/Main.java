20af8c67eff696c4dfa0718b5f054568c4f2356b
Anya <anya.chocolat@gmail.com>
2022-09-25 12:48:16 +0200

    Resetting

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 import classes.Character;
+import classes.Parties;
 import classes.Warrior;
+import classes.Wizard;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Warrior anya = new Warrior(1, "Anya", 3, true);
-        System.out.println("Hello world!");
-        List<Character> characters = new ArrayList<>();
-        characters.add(anya);
-        anya.attack();
-        Warrior pum = (Warrior) characters.get(0);
-        System.out.println(pum);
-        pum.attack();
+
     }
 }
--- src/classes/Warrior.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class Warrior extends Character implements Attack {
-    public Warrior(int id, String name, int hp, boolean isAlive) {
-        super(id, name, hp, isAlive);
-    }
-
-}
