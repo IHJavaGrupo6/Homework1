730ecc1e1005b1b12c3c5b2ef0d8223a61d4abf9
Anya <anya.chocolat@gmail.com>
2022-09-27 09:18:02 +0200

    Updated class Battle

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import classes.Character;
 import classes.Parties;
-import classes.Warrior;
+import classes.Warriors;
 import classes.Wizard;
 
 import java.util.ArrayList;
@@ -8,6 +8,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Character> party1 = new ArrayList<>();
+        party1.add(new Wizard("wizard1"));
+        Character character1 = party1.get(0);
+
+        //character1.attack();
+
+        Character playerOne = party1.get(0);
+
+        if (party1.get(0) instanceof Wizard){
+            playerOne = (Wizard) party1.get(0);
+        } else if (character1 instanceof Warriors) {
+            playerOne = (Warriors) party1.get(0);
+        } else {
+            System.out.println("Not a valid character");
+        }
+
+        //character1.attack();
+        (Wizard)playerOne.attack();
+
+
 
     }
 }
